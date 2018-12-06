51f3015389b6e3df0d7da6a0c991aefc088e6b9b
Henry Slawniak <meggawatts@gmail.com>
2018-12-06 12:52:28 -0600

    Get in the winter spirit

---
+++ client/static/snow.js
@@ -0,0 +1,85 @@
+class SnowFlake {
+  constructor () {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.radius = 0;
+    this.alpha = 0;
+    this.reset();
+    this.update();
+  }
+
+  reset() {
+    this.x = this.randBetween(0, window.innerWidth);
+    this.y = this.randBetween(0, -window.innerHeight);
+    this.vx = this.randBetween(-3, 3);
+    this.vy = this.randBetween(2, 5);
+    this.radius = this.randBetween(1, 4);
+    this.alpha = this.randBetween(0.1, 0.9);
+  }
+
+  randBetween(min, max) {
+    return min + Math.random() * (max - min);
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.y + this.radius > window.innerHeight) {
+      this.reset();
+    }
+  }
+}
+
+class Snow {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
+
+    window.addEventListener("resize", this.onResize());
+    this.onResize();
+    this.updateBound = this.update.bind(this);
+
+    this.createSnowFlakes();
+    requestAnimationFrame(this.updateBound);
+  }
+
+  onResize() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+  }
+
+  createSnowFlakes() {
+    const flakeCount = window.innerWidth / 2;
+    this.snowFlakes = [];
+
+    for (let i=0; i < flakeCount; i++) {
+      this.snowFlakes.push(new SnowFlake());
+    }
+  }
+
+  update() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+
+    for (const flake of this.snowFlakes) {
+      flake.update();
+
+      this.ctx.save();
+      this.ctx.fillStyle = "#FFF";
+      this.ctx.beginPath();
+      this.ctx.arc(flake.x, flake.y, flake.radius, 0, Math.PI * 2);
+      this.ctx.closePath();
+      this.ctx.globalAlpha = flake.alpha;
+      this.ctx.fill();
+      this.ctx.restore();
+    }
+    requestAnimationFrame(this.updateBound);
+  }
+}
+
+new Snow();
